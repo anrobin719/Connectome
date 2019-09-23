4bd2caf94a4fb7a3674ec726c288c6b69e84541a
안로빈 <anrobin719@gmail.com>
2019-09-23 23:31:20 +0900

    [fix] Modify post_card components responsive style

--- src/components/common/Navigation/NavigationItems/NavigationItems.js
+++ src/components/common/Navigation/NavigationItems/NavigationItems.js
@@ -1,42 +1,52 @@
-import React, { Fragment } from 'react';
-import NavigationItem from './NavigationItem/NavigationItem';
-import classes from './NavigationItems.scss'
-import Button from '../../../UI/Button/Button';
+import React, { Fragment } from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
+import classes from "./NavigationItems.scss";
+import Button from "../../../UI/Button/Button";
 
-const NavigationItems = ({onShowModal, onLogout, isAuthenticated}) => {
-    return (
-        <ul className={classes.NavigationItems}>
-            <NavigationItem link="/list" exact>재능 교환</NavigationItem>
+const NavigationItems = ({ onShowModal, onLogout, isAuthenticated }) => {
+  return (
+    <ul className={classes.NavigationItems}>
+      <NavigationItem link="/list" exact>
+        재능 교환
+      </NavigationItem>
 
-            {isAuthenticated ? (
-                <Fragment>
-                    <NavigationItem link="/edit" exact>새글 작성</NavigationItem>
-                    <NavigationItem link="/mypage" exact>마이 페이지</NavigationItem>
-                </Fragment>
-            ) : (
-                <Button onClick={() => onShowModal('askSignIn')} theme="nav">새글 작성</Button>
-            )}
+      {isAuthenticated ? (
+        <Fragment>
+          <NavigationItem link="/edit" exact>
+            새글 작성
+          </NavigationItem>
+          <NavigationItem link="/mypage" exact>
+            마이 페이지
+          </NavigationItem>
+        </Fragment>
+      ) : (
+        <Button onClick={() => onShowModal("askSignIn")} theme="nav">
+          새글 작성
+        </Button>
+      )}
 
-            {isAuthenticated ? (
-                <Button onClick={onLogout} theme="nav">
-                    <div>
-                        로그아웃
-                    </div>
-                </Button>
-            ) : (
-                <Fragment>
-                    <Button onClick={() => onShowModal('signIn')} theme="nav">
-                        <div>
-                            로그인
-                        </div>
-                    </Button>
-                    <div className={classes.signUpBtnBox}>
-                        <Button onClick={() => onShowModal('signUp')} theme="outlineNav" exact>회원가입</Button>
-                    </div>
-                </Fragment>
-            )}
-        </ul>
-    );
+      {isAuthenticated ? (
+        <Button onClick={onLogout} theme="nav">
+          <div>로그아웃</div>
+        </Button>
+      ) : (
+        <Fragment>
+          <Button onClick={() => onShowModal("signIn")} theme="nav">
+            <div>로그인</div>
+          </Button>
+          <div className={classes.signUpBtnBox}>
+            <Button
+              onClick={() => onShowModal("signUp")}
+              theme="outlineNav"
+              exact
+            >
+              회원가입
+            </Button>
+          </div>
+        </Fragment>
+      )}
+    </ul>
+  );
 };
 
 export default NavigationItems;
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,75 +1,75 @@
-import React, { Component, Fragment } from 'react';
-import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import React, { Component, Fragment } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
 
-import Toolbar from '../../components/common/Navigation/Toolbar/Toolbar';
-import SideDrawer from '../../components/common/Navigation/SideDrawer/SideDrawer';
-import Footer from '../../components/common/Footer/Footer';
-import * as actions from '../../store/actions/index';
-import classes from './Layout.scss';
-import { isatty } from 'tty';
+import Toolbar from "../../components/common/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/common/Navigation/SideDrawer/SideDrawer";
+import Footer from "../../components/common/Footer/Footer";
+import * as actions from "../../store/actions/index";
+import classes from "./Layout.scss";
 
 class Layout extends Component {
-    state = {
-        showDrawer: false
-    }
+  state = {
+    showDrawer: false
+  };
 
-    submitHandler = searchValue => {
-        const { history } = this.props;
-        if (searchValue.length !== 0) {
-            history.push(`/list/${searchValue}`);
-        }
+  submitHandler = searchValue => {
+    const { history } = this.props;
+    if (searchValue.length !== 0) {
+      history.push(`/list/${searchValue}`);
     }
+  };
 
-    sideDrawerToggleHandler = () => {
-        this.setState(prevState => {
-            return { showDrawer: !prevState.showDrawer };
-        });
-    }
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showDrawer: !prevState.showDrawer };
+    });
+  };
 
-    sideDrawerClosedHandler = () => {
-        this.setState({ showDrawer: false });
-    }
+  sideDrawerClosedHandler = () => {
+    this.setState({ showDrawer: false });
+  };
 
-    render() {
-        console.log(this.props.location);
-        const {onShowModal, onLogout, isAuthenticated} = this.props;
-        return (
-            <Fragment>
-                <SideDrawer
-                    onShowModal={onShowModal}
-                    onLogout={onLogout}
-                    isAuthenticated={isAuthenticated}
-                    show={this.state.showDrawer}
-                    hide={this.sideDrawerClosedHandler}
-                />
-                <Toolbar
-                    onShowModal={onShowModal}
-                    onLogout={onLogout}
-                    isAuthenticated={isAuthenticated}
-                    submitHandler={this.submitHandler}
-                    drawerToggleClicked={this.sideDrawerToggleHandler}
-                />
-                <main className={classes.LayoutMain}>
-                    {this.props.children}
-                </main>
-                <Footer />
-            </Fragment>
-        );
-    }
+  render() {
+    console.log(this.props.location);
+    const { onShowModal, onLogout, isAuthenticated } = this.props;
+    return (
+      <Fragment>
+        <SideDrawer
+          onShowModal={onShowModal}
+          onLogout={onLogout}
+          isAuthenticated={isAuthenticated}
+          show={this.state.showDrawer}
+          hide={this.sideDrawerClosedHandler}
+        />
+        <Toolbar
+          onShowModal={onShowModal}
+          onLogout={onLogout}
+          isAuthenticated={isAuthenticated}
+          submitHandler={this.submitHandler}
+          drawerToggleClicked={this.sideDrawerToggleHandler}
+        />
+        <main className={classes.LayoutMain}>{this.props.children}</main>
+        <Footer />
+      </Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        isAuthenticated: state.auth.token !== null
-    }
-}
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        onShowModal: (modalName) => dispatch(actions.showModal(modalName)),
-        onLogout: () => dispatch(actions.authLogout())
-    }
-}
+  return {
+    onShowModal: modalName => dispatch(actions.showModal(modalName)),
+    onLogout: () => dispatch(actions.authLogout())
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Layout));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withRouter(Layout));
