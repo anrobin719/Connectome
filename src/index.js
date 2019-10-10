1ab34d5ea90e5af6abc34f8c865e71b911af1b5c
안로빈 <anrobin719@gmail.com>
2019-10-10 21:31:08 +0900

    [chore] Register service-worker file

--- src/index.js
+++ src/index.js
@@ -1,51 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { StylesProvider } from "@material-ui/styles";
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import ScrollToTop from './hoc/ScrollToTop/ScrollToTop';
-import editorReducer from './store/reducers/editor';
-import postReducer from './store/reducers/post';
-import listReducer from './store/reducers/list';
-import baseReducer from './store/reducers/base';
-import authReducer from './store/reducers/auth';
-import mypageReducer from './store/reducers/mypage';
-
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import ScrollToTop from "./hoc/ScrollToTop/ScrollToTop";
+import {
+  editorReducer,
+  postReducer,
+  listReducer,
+  baseReducer,
+  authReducer,
+  mypageReducer
+} from "./store/reducers/index";
 
 const rootReducer = combineReducers({
-    editor: editorReducer,
-    post: postReducer,
-    list: listReducer,
-    base: baseReducer,
-    auth: authReducer,
-    mypage: mypageReducer
+  editor: editorReducer,
+  post: postReducer,
+  list: listReducer,
+  base: baseReducer,
+  auth: authReducer,
+  mypage: mypageReducer
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
-));
-
-const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <StylesProvider injectFirst>
-                <ScrollToTop>
-                    <App />
-                </ScrollToTop>
-            </StylesProvider>
-        </BrowserRouter>
-    </Provider>
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
 );
 
-ReactDOM.render(app, document.getElementById('root'));
+const app = (
+  <Provider store={store}>
+    <BrowserRouter>
+      <StylesProvider injectFirst>
+        <ScrollToTop>
+          <App />
+        </ScrollToTop>
+      </StylesProvider>
+    </BrowserRouter>
+  </Provider>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(app, document.getElementById("root"));
+serviceWorker.register();
---
+++ src/store/reducers/index.js
@@ -0,0 +1,6 @@
+export { default as authReducer } from "./auth";
+export { default as baseReducer } from "./base";
+export { default as editorReducer } from "./editor";
+export { default as listReducer } from "./list";
+export { default as mypageReducer } from "./mypage";
+export { default as postReducer } from "./post";
